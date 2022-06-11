13d2e7e55d264ee4dfa001ef4bee0c1f12b4407f
Nguyen Minh Châu <91469379+light1406@users.noreply.github.com>
2022-06-11 21:32:40 +0700

    update nav 1.0 and splash screen

--- app/src/main/java/com/example/comic/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/comic/ui/activity/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.comic.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -9,15 +13,33 @@ import com.example.comic.dao.ComicDao;
 import com.example.comic.obj.Chapter;
 import com.example.comic.obj.Comic;
 import com.example.comic.obj.ComicImage;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppBarConfiguration appBarConfiguration;
+    private NavController controller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(findViewById(R.id.tool_bar));
+
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.home_fragment)
+                .setOpenableLayout(findViewById(R.id.drawer))
+                .build();
+        controller = Navigation.findNavController(this, R.id.nav_control);
+        NavigationView navigationView = findViewById(R.id.nav);
+        NavigationUI.setupActionBarWithNavController(this, controller, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, controller);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(controller, appBarConfiguration);
     }
 }
---
+++ app/src/main/java/com/example/comic/ui/activity/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.comic.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.comic.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1500);
+    }
+}
